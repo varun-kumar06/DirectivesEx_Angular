86c729f4acf01063572d912d09d36bfab30bc3e7
varun-kumar06 <kumarmvarun39@gmail.com>
2024-10-05 16:26:40 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from './employee';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DirectivesEx';
+  nums=[24,6,98,83,55]
+  name1="";
+  names:String []=[];
+   age1: any;
+   location1: any;
+  addList(){
+    this.names.push(this.name1);
+  }
+
+  name:String;
+  age:number;
+  location:String;
+
+  emp : Employee;
+  emps : Employee[]=[];
+
+  submitEmp(){
+  this.emp= new Employee(this.name,this.age,this.location);
+    this.emps.push(this.emp);
+    console.log(this.emps);
+  
+  }
+
+  pushEmp(){
+    this.emps.push(this.emp);
+   
+  }
+
+  isvisible=true;
+  hideList(){
+    this.isvisible=!this.isvisible;
+  }
+
+  isshow=true;
+  show(){
+    this.isshow=!this.isshow;
+  }
+
+  day=0;
+
+dy=false;
+days(){
+  this.dy=!this.dy;
+}
+arr=["John","Sam","Joy"];
+
+
+
 }
---
+++ src/app/employee.ts
@@ -0,0 +1,11 @@
+export class Employee {
+    name:String;
+    age:number;
+    location:String;
+
+    constructor(name:String,age:number,location:String){
+        this.name=name;
+        this.age=age;
+        this.location=location;
+    }
+}
